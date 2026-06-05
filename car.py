3204cabdccc1aac0388cde78e323e0aeb5952cbd
Dumindu Nadeera <omdnadeera@gmail.com>
2026-06-05 12:29:08 +0530

    Implement car class with display and accelerate methods
    Fix class definition and method calls in car.py

---
+++ car.py
@@ -0,0 +1,15 @@
+class car:
+
+	def car(self, self.brand, self.speed):
+		self.brand = brand
+		self.speed = speed
+
+	def display(self):
+		printf("Car is {self.brand} & speed {self.speed}")
+
+	def accelarate(self,speed):
+		self.speed = self.speed + speed
+
+my_car = car("Toyota", 60)
+mycar.display()
+				
